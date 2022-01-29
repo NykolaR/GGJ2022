15aa0b6419673849bd9612e23da683b83c023c1a
NykolaR <thenykreed@gmail.com>
2022-01-29 11:28:31 -0600

    Boat 'n water

--- Scenes/Boat/Boat.gd
+++ Scenes/Boat/Boat.gd
@@ -2,8 +2,13 @@ extends RigidBody
 
 # ray length = 2
 
-onready var cast_holder : Spatial = $CastHolder
-onready var casts : Array
+const FORCE_SCALE : float = 4.0# * 10.0
+
+onready var area : Area = $Area as Area
+onready var cast_holder : Spatial = $CastHolder as Spatial
+var casts : Array
+
+export var force_curve : Curve
 
 func _ready() -> void:
 	casts = cast_holder.get_children()
@@ -14,6 +19,15 @@ func _physics_process(delta: float) -> void:
 		
 		if rcast.is_colliding():
 			var col_distance : float = rcast.global_transform.origin.distance_to(rcast.get_collision_point())
-			add_force(Vector3.ZERO, rcast.transform.origin)
+			# distance range 1.25 - 0
+			col_distance = clamp(col_distance, 2.75, 3.25)
+			# lower = more force
+			col_distance = range_lerp(col_distance, 2.75, 3.25, 1, 0)
+			# switch to interpolate baked once curve is finalized
+			add_force(Vector3.UP * force_curve.interpolate(col_distance) * FORCE_SCALE, rcast.transform.origin)
 
+func _body_entered(body: Node) -> void:
+	linear_damp = 2.0
 
+func _body_exited(body: Node) -> void:
+	linear_damp = 1.0
---
+++ Scenes/Water/Water.gd
@@ -0,0 +1,43 @@
+extends Spatial
+
+const MAP_SCALE : int = 60
+const VIEWPORT_STEP : float = 256.0 / 60.0
+
+var offset : Vector2 = Vector2() setget set_offset
+var wind_direction : Vector2 = Vector2.UP
+var wind_speed : float = 0.01
+var wave_height : float = 1.0
+
+onready var heightmap_viewport : Viewport = $Viewport
+onready var water_shader = $Viewport/Sprite.material as ShaderMaterial
+onready var heightmap : HeightMapShape = $StaticBody/CollisionShape.shape as HeightMapShape
+
+#func _ready() -> void:
+#	water_shader = $Viewport/Sprite.material as ShaderMaterial
+#	water_shader.set_shader_param("noise2", $Viewport.get_texture())
+
+func _physics_process(delta: float) -> void:
+	update_heightmap()
+
+func _process(delta: float) -> void:
+	set_offset(offset + wind_direction * wind_speed * delta)
+
+func update_heightmap() -> void:
+	var n_float_array : PoolRealArray = PoolRealArray()
+	n_float_array.resize(MAP_SCALE * MAP_SCALE)
+	var data : Image = heightmap_viewport.get_texture().get_data()
+	data.lock() # can the image be permanently stored instead of allocated each frame?
+	
+	var i : int = 0
+	for y in MAP_SCALE:
+		for x in MAP_SCALE:
+			var h : float = data.get_pixel(x * VIEWPORT_STEP, y * VIEWPORT_STEP).r
+			n_float_array[i] = h * wave_height
+			i += 1
+	
+	data.unlock()
+	heightmap.map_data = n_float_array
+
+func set_offset(new : Vector2) -> void:
+	offset = new
+	water_shader.set_shader_param("offset", offset)
