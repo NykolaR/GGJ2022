aad85056ce68fac30e4df0a85eeee53581a13acc
Adam Tilson <tilson3a@uregina.ca>
2022-01-30 11:03:17 -0600

    waves

--- Scenes/Spawners/Spawner.gd
+++ Scenes/Spawners/Spawner.gd
@@ -17,14 +17,19 @@ func _ready():
 func _process(delta):
 	print(intensity)
 	if (intensity < STORM_1):
+		# Just Waves
 		pass
 	elif (intensity >= STORM_1 and intensity >= STORM_2):
+		# Just Kraken
 		pass
 	elif (intensity >= STORM_2 and intensity >= STORM_3):
+		# Kraken and Waves but not at the same time?
 		pass
 	elif (intensity >= STORM_3 and intensity >= STORM_4):
+		# Occasional weaves, and heavy kraken
 		pass
 	elif (intensity == STORM_4):
+		# Send waves from all directions, and occasional kraken
 		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
