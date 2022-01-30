57884f124227bfd2c767dd27279fe7366ad9a27c
NykolaR <thenykreed@gmail.com>
2022-01-30 10:49:52 -0600

    game flow

--- Scenes/Game/Main.gd
+++ Scenes/Game/Main.gd
@@ -4,12 +4,12 @@ const BOAT : PackedScene = preload("res://Scenes/Boat/Boat.tscn")
 onready var boat_spawn : Spatial = $BoatSpawn
 
 enum {MAIN, GAME, OVER}
-var state : int = MAIN setget set_state
+var state : int = -1 setget set_state
 enum {CALM, STORM}
 var weather_state : int = CALM setget set_weather
 
 onready var main_menu : Control = $MainMenu as Control
-onready var game_over : Control = $MainMenu as Control
+onready var game_over : Control = $GameOver as Control
 onready var water : Spatial = $Water as Spatial
 onready var tween : Tween = $Tween as Tween
 onready var animation : AnimationPlayer = $AnimationPlayer as AnimationPlayer
@@ -19,15 +19,23 @@ var intensity : float = 0.0 setget set_intensity
 # waves last from 30s to 120s
 var current_wave : int = 0
 
+func _ready() -> void:
+	animation.play("Intensity")
+	animation.stop(false)
+	set_state(MAIN)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire_ye_cannon"):
 		match state:
 			MAIN:
-				set_state(GAME)
+				if main_menu.modulate.a >= 0.9 and not tween.is_active():
+					set_state(GAME)
+					start_game()
 			GAME:
 				pass
 			OVER:
-				set_state(MAIN)
+				if game_over.modulate.a >= 0.9 and not tween.is_active():
+					set_state(MAIN)
 
 func start_game() -> void:
 	var nboat : Spatial = BOAT.instance()
@@ -40,9 +48,6 @@ func start_game() -> void:
 	state_flip.wait_time = 8 # (?)
 	state_flip.start()
 
-func ship_sunk() -> void:
-	pass
-
 func end_game() -> void:
 	state_flip.stop()
 	set_weather(CALM)
@@ -54,11 +59,17 @@ func set_state(new : int) -> void:
 	state = new
 	match state:
 		MAIN:
-			pass
+			tween.interpolate_property(main_menu, "modulate:a", null, 1.0, 3.0, Tween.TRANS_SINE, Tween.EASE_IN)
+			tween.interpolate_property(game_over, "modulate:a", null, 0.0, 1.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+			tween.start()
 		GAME:
-			pass
+			tween.interpolate_property(main_menu, "modulate:a", null, 0.0, 1.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+			tween.interpolate_property(game_over, "modulate:a", null, 0.0, 1.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+			tween.start()
 		OVER:
-			pass
+			tween.interpolate_property(game_over, "modulate:a", null, 1.0, 3.0, Tween.TRANS_SINE, Tween.EASE_IN)
+			tween.interpolate_property(main_menu, "modulate:a", null, 0.0, 1.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+			tween.start()
 
 func set_intensity(new : float) -> void:
 	intensity = clamp(new, 0, 1)
@@ -77,10 +88,11 @@ func set_weather(new : int) -> void:
 	weather_state = new
 	match weather_state:
 		CALM:
-			tween.interpolate_property(self, "intensity", null, 0.0, 3.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+			var nintensity : float = rand_range(0.0, 0.1)
+			tween.interpolate_property(self, "intensity", null, nintensity, 3.0, Tween.TRANS_QUAD, Tween.EASE_IN)
 			tween.start()
 		STORM:
-			var nintensity : float = 0.5
+			var nintensity : float = clamp(0.2 + current_wave * 0.2, 0, 1)
 			var time : float = 10.0
 			tween.interpolate_property(self, "intensity", null, nintensity, time, Tween.TRANS_CUBIC, Tween.EASE_IN)
 			tween.start()
