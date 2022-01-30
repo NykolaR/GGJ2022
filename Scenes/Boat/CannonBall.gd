4391361fd4110055bc086caf8c09586a84ded1c2
NykolaR <thenykreed@gmail.com>
2022-01-29 21:56:13 -0600

    idunno

--- Scenes/Boat/CannonBall.gd
+++ Scenes/Boat/CannonBall.gd
@@ -1,5 +1,11 @@
 extends RigidBody
 
+onready var shot_sounds : Spatial = $Shot
+
 func _ready() -> void:
 	apply_central_impulse(transform.basis.z*-1)
+	
+	shot_sounds.get_child(randi()%8).play()
 
+func _screen_exited() -> void:
+	pass # Replace with function body.
